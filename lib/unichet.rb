b8951419afadb8529045d8167a704667c922a54a
Nikita <Nikita>
2024-12-15 11:27:22 +0300

    gem initializing

---
+++ lib/unichet.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "unichet/version"
+
+module Unichet
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/unichet_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Unichet do
+  it "has a version number" do
+    expect(Unichet::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
